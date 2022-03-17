1126774b207f174e736861bfa145698997ad2c77
alexis anzieu <alexis.anzieu@ssense.com>
2022-03-17 18:22:00 -0400

    update

--- app/routes/auth/$provider.callback.tsx
+++ app/routes/auth/$provider.callback.tsx
@@ -4,6 +4,5 @@ import { authenticator } from "~/utils/auth.server";
 export let loader: LoaderFunction = ({ request, params }: any) => {
     return authenticator.authenticate(params.provider, request, {
         successRedirect: "/",
-        failureRedirect: "/",
     });
 };
