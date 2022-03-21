7b56a542df216dc53eff7c75e89287a0f51261b8
alexis anzieu <alexis.anzieu@ssense.com>
2022-03-21 16:28:31 -0400

    less strict

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -7,7 +7,6 @@ export let sessionStorage = createCookieSessionStorage({
         name: "_session", // use any name you want here
         sameSite: "lax", // this helps with CSRF
         path: "/", // remember to add this so the cookie will work in all routes
-        httpOnly: true, // for security reasons, make this cookie http only
         secure: process.env.NODE_ENV === "production", // enable this in prod only
     },
 });
