b51893e388ce1462f9230d5551dd61eb2247a2eb
alexis anzieu <alexis.anzieu@ssense.com>
2022-03-21 16:21:21 -0400

    remove secret

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,9 +1,14 @@
 import { createCookieSessionStorage } from "remix";
+import "dotenv/config";
 
 // export the whole sessionStorage object
 export let sessionStorage = createCookieSessionStorage({
     cookie: {
         name: "_session", // use any name you want here
+        sameSite: "lax", // this helps with CSRF
+        path: "/", // remember to add this so the cookie will work in all routes
+        httpOnly: true, // for security reasons, make this cookie http only
+        secure: process.env.NODE_ENV === "production", // enable this in prod only
     },
 });
 
