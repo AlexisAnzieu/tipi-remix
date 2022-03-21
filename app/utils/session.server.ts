bf9461a72aa5f0a002b23564652eacda4ccab2bb
alexis anzieu <alexis.anzieu@ssense.com>
2022-03-21 15:07:32 -0400

    laxist cookie

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -4,10 +4,10 @@ import { createCookieSessionStorage } from "remix";
 export let sessionStorage = createCookieSessionStorage({
     cookie: {
         name: "_session", // use any name you want here
-        sameSite: "lax", // this helps with CSRF
+        sameSite: "none", // this helps with CSRF
         path: "/", // remember to add this so the cookie will work in all routes
         httpOnly: true, // for security reasons, make this cookie http only
-        secrets: ["s3cr3t"], // replace this with an actual secret
+        secrets: ["newsecret"], // replace this with an actual secret
         secure: false, // enable this in prod only
     },
 });
