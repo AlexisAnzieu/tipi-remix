54c9b88d008ddfdbc42f00bdb7a11bcdcdb555de
alexis anzieu <alexis.anzieu@ssense.com>
2022-03-21 16:13:14 -0400

    test other cookie type

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -3,12 +3,7 @@ import { createCookieSessionStorage } from "remix";
 // export the whole sessionStorage object
 export let sessionStorage = createCookieSessionStorage({
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 * 7 * 52, // 1 year
         name: "tipisession", // use any name you want here
-        sameSite: "lax", // this helps with CSRF
-        path: "/", // remember to add this so the cookie will work in all routes
-        httpOnly: true, // for security reasons, make this cookie http only
-        secure: process.env.NODE_ENV === "production", // enable this in prod only
     },
 });
 
