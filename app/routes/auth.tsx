4dd716da3c8b54e6e4956d36c3ca59f026aa24e5
alexis anzieu <alexis.anzieu@ssense.com>
2022-04-14 12:20:02 -0400

    avoid issue

--- app/routes/auth.tsx
+++ app/routes/auth.tsx
@@ -11,15 +11,15 @@ export default function Auth({ users }: any) {
     return (
         <>
             <div style={{ textAlign: "center", paddingBottom: "2em" }}>
-            <h5>PASS restants : > 30</h5>
+                <h5>{"PASS restants : > 30"}</h5>
 
-               {/* <h5>PASS restants [{100 - users.length} / 100]</h5> */}
+                {/* <h5>PASS restants [{100 - users.length} / 100]</h5> */}
                 <LoadingBars
                     animator={{ activate: true }}
                     determinate
                     progress={users.length + 30}
                 />
-            </div> 
+            </div>
             <FrameHexagon
                 animator={{
                     duration: {
