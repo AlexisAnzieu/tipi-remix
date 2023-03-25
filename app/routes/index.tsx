10b472639dd3505273df1f9fc243f825d238b8f0
alexis anzieu <alexis.anzieu@ssense.com>
2023-03-25 14:38:32 -0400

    add histoire

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -68,10 +68,43 @@ export default function Index() {
                         height: "100%",
                     }}
                 >
-                    Un an s'est écoulé depuis que la comète Normand1LX32 s’est
-                    écrasée en Lanaudière, l'équipe de scientifiques s'est
-                    maintenant subdivisée en sept comités afin d'organiser ce
-                    nouveau campement, sur le thème de la fête foraine.
+                    Un an après s'être établi, le groupe de mercenaires
+                    scientifique éprouvait le besoin de se ressourcer et de
+                    renforcer sa cohésion. Ainsi, ils eurent l'ingénieuse idée
+                    d'organiser un campement dans la forêt afin de concevoir une
+                    fête foraine. <br /> <br />
+                    Les scientifiques se mirent aussitôt à la tâche pour
+                    planifier cet événement. Chacun proposa une activité qu'il
+                    souhaitait mettre en place, créant ainsi une liste
+                    d'attractions. Ils construisirent des manèges, des stands de
+                    nourriture et des jeux pour le divertissement des visiteurs.
+                    <br /> <br /> Au commencement, les scientifiques eurent des
+                    difficultés à se retrouver dans ce nouvel environnement, car
+                    ils étaient habitués à évoluer en laboratoire et avaient
+                    peine à s'abandonner au divertissement. Cependant, ils
+                    découvrirent rapidement que travailler de concert à
+                    l'élaboration de la fête renforçait leur cohésion et leur
+                    complicité. <br /> <br /> La fête foraine remporta un grand
+                    succès. Les scientifiques accueillirent des visiteurs venus
+                    de l'ensemble du pays et proposèrent des jeux et des manèges
+                    captivants. Les visiteurs furent ravis de rencontrer des
+                    scientifiques et de découvrir les dessous de leur travail.
+                    Les scientifiques se réjouirent de partager leur passion.{" "}
+                    <br />
+                    <br /> Le soir venu, les scientifiques se rassemblèrent
+                    autour d'un feu de camp et échangèrent sur la façon dont la
+                    fête avait renforcé leur cohésion et leur avait permis de se
+                    ressourcer. Ils convirent que la création d'une fête foraine
+                    était un excellent moyen de renforcer leur amitié et de
+                    partager leur amour pour la science avec le monde. <br />{" "}
+                    <br />
+                    Lorsqu'arriva la fin du week-end, les scientifiques
+                    démontèrent les manèges et les stands de nourriture,
+                    éteignirent les lumières et quittèrent leur campement dans
+                    la forêt. Toutefois, ils étaient convaincus que cette
+                    expérience demeurerait à jamais gravée dans leur mémoire et
+                    avait renforcé leur camaraderie et leur esprit d'équipe pour
+                    les années à venir.
                 </Text>
                 <img
                     style={{
