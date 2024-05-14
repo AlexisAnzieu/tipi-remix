9b7d8ff3a5b5d6c71282b0429b00285690334bc4
alexis <alexis.anzieu@gmail.com>
2024-05-13 23:08:38 -0400

    lets go new v

--- app/root.tsx
+++ app/root.tsx
@@ -1,11 +1,11 @@
 import { type MetaFunction } from "@remix-run/node";
 import {
-    Links,
-    LiveReload,
-    Meta,
-    Outlet,
-    Scripts,
-    ScrollRestoration,
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration,
 } from "@remix-run/react";
 import styles from "~/style.css";
 import { ArwesThemeProvider, StylesBaseline } from "@arwes/core";
@@ -15,60 +15,57 @@ const FONT_FAMILY_ROOT = '"Titillium Web", sans-serif';
 const FONT_FAMILY_CODE = '"Source Code Pro", monospace';
 
 const animatorGeneral = {
-    duration: { enter: 200, exit: 200, stagger: 30 },
+  duration: { enter: 200, exit: 200, stagger: 30 },
 };
 
 export const meta: MetaFunction = () => {
-    return {
-        title: "Portail intergalactique",
-        description: "Interface de recherche intergalactique TiPi",
-        "og:image": "./images/tipi.jpg",
-    };
+  return {
+    title: "Portail intergalactique",
+    description: "Interface de recherche intergalactique TiPi",
+    "og:image": "./images/pyramid.jpg",
+  };
 };
 
 export function links() {
-    return [
-        {
-            rel: "stylesheet",
-            href: styles,
-        },
-        {
-            rel: "icon",
-            href: "/favicon.png",
-        },
-    ];
+  return [
+    {
+      rel: "stylesheet",
+      href: styles,
+    },
+    {
+      rel: "icon",
+      href: "/favicon.png",
+    },
+  ];
 }
 export default function App() {
-    return (
-        <html lang="en">
-            <head>
-                <meta charSet="utf-8" />
-                <meta
-                    name="viewport"
-                    content="width=device-width,initial-scale=1"
-                />
-                <Meta />
-                <Links />
-            </head>
-            <body>
-                <ArwesThemeProvider>
-                    <StylesBaseline
-                        styles={{
-                            "html, body": {
-                                fontFamily: FONT_FAMILY_ROOT,
-                                padding: "20px",
-                            },
-                            "code, pre": { fontFamily: FONT_FAMILY_CODE },
-                        }}
-                    />
-                    <AnimatorGeneralProvider animator={animatorGeneral}>
-                        <Outlet />
-                    </AnimatorGeneralProvider>
-                </ArwesThemeProvider>
-                <ScrollRestoration />
-                <Scripts />
-                {process.env.NODE_ENV === "development" && <LiveReload />}
-            </body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width,initial-scale=1" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <ArwesThemeProvider>
+          <StylesBaseline
+            styles={{
+              "html, body": {
+                fontFamily: FONT_FAMILY_ROOT,
+                padding: "20px",
+              },
+              "code, pre": { fontFamily: FONT_FAMILY_CODE },
+            }}
+          />
+          <AnimatorGeneralProvider animator={animatorGeneral}>
+            <Outlet />
+          </AnimatorGeneralProvider>
+        </ArwesThemeProvider>
+        <ScrollRestoration />
+        <Scripts />
+        {process.env.NODE_ENV === "development" && <LiveReload />}
+      </body>
+    </html>
+  );
 }
