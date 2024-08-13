a54459f53213b8980d9980ce340a8cc7cadbf1e5
alexis <alexis.anzieu@gmail.com>
2024-08-13 15:29:26 -0400

    push new frequences

--- app/routes/frequences.tsx
+++ app/routes/frequences.tsx
@@ -9,63 +9,57 @@ import { Button, Table } from "@arwes/core";
 
 const artists = [
   {
-    name: "libre accès",
-    schedule: "21:00",
-    type: "toutes",
-    day: "friday",
-  },
-  {
-    name: "Long Dail",
-    schedule: "23:00",
-    type: "Break House",
-    day: "friday",
-  },
-  {
-    name: "Perle",
-    schedule: "00:00",
-    type: "Acid House",
-    day: "friday",
-  },
-  {
-    name: "Jennifer System",
-    schedule: "01:30",
-    type: "Electronic",
+    name: "Sunset Mix",
+    schedule: "18:30 - 20h",
+    type: "Deep House",
     day: "friday",
   },
   {
     name: "Jave Marlou",
-    schedule: "02:30",
-    type: "Raw House",
+    schedule: "22:00",
+    type: "Disco / House",
     day: "friday",
   },
   {
     name: "L2",
-    schedule: "04:00",
-    type: "Techno",
+    schedule: "00:00",
+    type: "House / Techno",
     day: "friday",
   },
   {
-    name: "libre accès",
-    schedule: "21:00",
-    type: "toutes",
+    name: "(FR)eezer",
+    schedule: "02:30 - 5h",
+    type: "Exposé électronique",
+    day: "friday",
+  },
+  {
+    name: "Sunset Mix",
+    schedule: "18:30 - 20h",
+    type: "Deep house",
     day: "saturday",
   },
   {
-    name: "Dj Jounette ",
-    schedule: "22:30",
-    type: "Tribal",
+    name: "Nauemany ",
+    schedule: "22:00",
+    type: "Tech House",
     day: "saturday",
   },
   {
-    name: "Blondie b2b Elena",
+    name: "Pierre",
     schedule: "23:30",
-    type: "Techno",
+    type: "Progressive House",
     day: "saturday",
   },
   {
-    name: "(fr)eezer",
-    schedule: "01:30",
-    type: "Closing éclectique",
+    name: "Jave Marlou b2b (FR)eezer",
+    schedule: "01:00",
+    type: "Dark disco",
+    day: "saturday",
+  },
+  {
+    name: "Kroma",
+    schedule: "02:30 - 4h",
+    type: "Techno",
     day: "saturday",
   },
 ];
@@ -82,8 +76,8 @@ const dataset = (day: string) =>
       id: index,
       columns: [
         { id: "p", data: artist.schedule },
-        { id: "q", data: "en cours d'analyse..." },
-        { id: "z", data: "en cours d'analyse..." },
+        { id: "q", data: artist.name },
+        { id: "z", data: artist.type },
       ],
     }));
 export default function Frequences() {
@@ -94,14 +88,14 @@ export default function Frequences() {
       </Link>
       <br />
       <br />
-      <h3>Samedi (21h-5h)</h3>
+      <h3>Samedi (22h-5h)</h3>
       <Table
         animator={{ animate: false }}
         headers={headers}
         dataset={dataset("friday")}
         columnWidths={["27%", "37%", "36%"]}
       />
-      <h3>Dimanche (21h-3h30)</h3>
+      <h3>Dimanche (22h-4h)</h3>
       <Table
         animator={{ animate: false }}
         headers={headers}
