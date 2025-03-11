3d36729b4cdd53a3e77c9593942f486ed2d773a6
alexis <alexis.anzieu@gmail.com>
2025-03-11 12:08:07 -0400

    remove unnecessary text from Billetterie component

--- app/routes/billetterie.tsx
+++ app/routes/billetterie.tsx
@@ -12,15 +12,6 @@ export default function Billetterie() {
       <h3>Billetterie </h3>
       <br />
       <br />
-      <FrameBox animator={{ activate: true }} linesWidths={[2, 0, 4, 0]}>
-        Au moment de payer votre billet, vous serez amenés à participer à
-        l'organisation de l'événement en{" "}
-        <Link to="/comites">rejoignant un des trois comités</Link>. Vous pouvez
-        aussi choisir de n'en rejoindre aucun, c'est vous qui décidez!
-      </FrameBox>
-      <br />
-      <br />
-      <br />
 
       <div
         style={{
