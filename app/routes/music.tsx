c4549da29a181389d7beda568dbd8c2502230507
Alexis anzieu <alexis.anzieu@ssense.com>
2025-06-23 14:58:23 -0400

    mega

--- app/routes/music.tsx
+++ app/routes/music.tsx
@@ -1,9 +1,18 @@
 export default function Music() {
+  const audioUrl =
+    "https://mega.nz/embed/oZ1HgBxI#JOP58mG6WGuIV92UoREtaySENNqBDmuRBRq3pWXiOI8!1v";
+
   return (
     <div className="music-container">
       <h1>DJ Sets</h1>
-      <div className="audio-player">
-        <audio controls src="/music/TDAudio.0.mp3" />
+      <div className="mega-embed">
+        <iframe
+          src={audioUrl}
+          frameBorder="0"
+          width="711"
+          height="144"
+          allowFullScreen
+        />
       </div>
     </div>
   );
